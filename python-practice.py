49facea2afdb42cbf49ae84a29cf7cfeb8ea91d6
nazar301 <nazar.basistyy@gmail.com>
2022-06-30 19:40:29 -0400

    puzzle problems

--- python-practice.py
+++ python-practice.py
@@ -1,1 +1,54 @@
-print('hello')
+# print('hello world')
+
+#printing practice 
+
+# print('Day 1 - Python Print Function')
+# print('The function is declared like this:')
+# print('print(\'what to print\')')
+
+
+# quick debugging session
+
+# print("Day 1 - String Manipulation")
+# print("String Concatenation is done with the \"+\" sign.")
+# print('e.g. print("Hello " + "world")')
+# print("New lines can be created with a backslash and n.")
+
+
+# concatination
+
+name = 'what is your name?'
+new = 'hi,'
+
+#print(new + " " + name)
+
+#new lines 
+
+# print('hello world! \nhello world! \nhello world! ')
+
+
+# return the length of the input 
+# when theres no name given default is the sentance, prints 16
+
+
+# string = input('What is your name?')
+# print('Hello ' + string + '!')
+# print(len(string))
+
+
+# variable change 
+
+# a = input('a: ')
+# b = input('b: ')
+
+# print(a)
+# print(b)
+
+# c=a
+# d=b
+
+# b=c
+# a=d
+
+# print(a)
+# print(b)
